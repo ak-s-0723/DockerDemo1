a9335b7ffc15fcb642969c588cda94a22cbfce14
khannaanurag1 <khannaanurag1@gmail.com>
2024-09-26 19:46:40 +0530

    dockerfile and testcontroller

---
+++ src/main/java/org/example/dockerdemo1/controllers/TestController.java
@@ -0,0 +1,16 @@
+package org.example.dockerdemo1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("{name}")
+    public String getName(@PathVariable String name) {
+        return name;
+    }
+}
